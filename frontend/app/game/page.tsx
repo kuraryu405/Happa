cba4e2ec2f880198c3fc33f1fbd9c6200285f300
くらりゅ！ <s1f102501260@iniad.org>
2026-02-24 11:01:49 +0900

    リザルト画面作成

--- frontend/app/game/page.tsx
+++ frontend/app/game/page.tsx
@@ -3,30 +3,10 @@
 export default function GamePage() {
   return (
     <>
-      <main className="flex flex-col items-center w-full max-w-2xl mx-auto pt-16 px-8">
-        <div className="chat chat-start w-full">
-          <div className="chat-bubble text-2xl px-8 py-6">
-            claudeはくらうで、codexはこでこでと呼ぶべきである。
-          </div>
+        {/* 質問設定 */}
+        <div className="flex items-center justify-center w-full h-auto p-4">
+          <textarea placeholder="Accent" className="textarea textarea-accent textarea-lg border border-base-300"></textarea>
         </div>
-        <div className="w-full pt-10">
-          <div className="skeleton h-32 w-full rounded-xl flex items-center justify-center gap-5">
-            <div className="flex-1 flex justify-center">
-              <div
-                className="radial-progress"
-                style={{ "--value": 70 } as React.CSSProperties}
-                aria-valuenow={70}
-                role="progressbar"
-              >
-                70%
-              </div>
-            </div>
-            <div className="flex-1 flex justify-center">
-              <p className="text-2xl font-bold">YES: null人</p>
-            </div>
-          </div>
-        </div>
-      </main>
     </>
   );
 }
