8c644dc8083e2e36871db55d06e02cd8e8f10897
くらりゅ！ <s1f102501260@iniad.org>
2026-03-05 22:09:25 +0900

    HOMEのボタンへのリンク追加

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -50,43 +50,41 @@ export default function RootLayout({
             Happa
           </Link>
         </header>
-        <div className="pt-16 pb-20">
-          {children}
-        </div>
+        <div className="pt-16 pb-20">{children}</div>
         <div className="dock z-50">
-    <button>
-      <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-        <g fill="currentColor" strokeLinejoin="miter" strokeLinecap="butt">
-          <polyline
-            points="1 11 12 2 23 11"
-            fill="none"
-            stroke="currentColor"
-            strokeMiterlimit="10"
-            strokeWidth="2"
-          ></polyline>
-          <path
-            d="m5,13v7c0,1.105.895,2,2,2h10c1.105,0,2-.895,2-2v-7"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="square"
-            strokeMiterlimit="10"
-            strokeWidth="2"
-          ></path>
-          <line
-            x1="12"
-            y1="22"
-            x2="12"
-            y2="18"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="square"
-            strokeMiterlimit="10"
-            strokeWidth="2"
-          ></line>
-        </g>
-      </svg>
-      <span className="dock-label">Home</span>
-    </button>
+          <Link href="/">
+            <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <g fill="currentColor" strokeLinejoin="miter" strokeLinecap="butt">
+                <polyline
+                  points="1 11 12 2 23 11"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeMiterlimit="10"
+                  strokeWidth="2"
+                ></polyline>
+                <path
+                  d="m5,13v7c0,1.105.895,2,2,2h10c1.105,0,2-.895,2-2v-7"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="square"
+                  strokeMiterlimit="10"
+                  strokeWidth="2"
+                ></path>
+                <line
+                  x1="12"
+                  y1="22"
+                  x2="12"
+                  y2="18"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="square"
+                  strokeMiterlimit="10"
+                  strokeWidth="2"
+                ></line>
+              </g>
+            </svg>
+            <span className="dock-label">Home</span>
+          </Link>
 
     <button>
       <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
